95f3c74bf4367bb367b703cbdf2012da2e0a6c24
jk315 <janakliem@gmx.net>
2018-09-23 17:37:36 +0200

    Skripts Pong
    Skripts zum Pong-Spiel, Jana Kliem

---
+++ SkriptsJK/Stocki.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stocki : MonoBehaviour {
+
+	float speed = 5f;
+	public bool isBump1;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (isBump1) {
+			if (Input.GetKey ("up") && transform.position.y < 5.3) {
+				transform.Translate (Vector3.up * speed * Time.deltaTime);
+			}
+
+			if (Input.GetKey ("down") && transform.position.y > -3.1) {
+				transform.Translate (Vector3.down * speed * Time.deltaTime);
+			}
+		} else {
+			GameObject ballGameObject = GameObject.Find ("Ball");
+		}
+		/*else {
+			if (Input.GetKey ("w") && transform.position.y < 5.3) {
+				transform.Translate (Vector3.up * speed * Time.deltaTime);
+			}
+
+			if (Input.GetKey ("s")&& transform.position.y > -3.1) {
+				transform.Translate (Vector3.down * speed * Time.deltaTime);
+			}
+		}*/
+	}
+}
